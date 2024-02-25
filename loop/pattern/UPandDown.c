8dd73f6bc35c5649574053a3a6f224d8a4f76dcf
ajeetyadaw108@gmail.com <ajeetyadaw108@gmail.com>
2024-02-25 13:26:25 +0530

    C code prectice

--- loop/pattern/UPandDown.c
+++ loop/pattern/UPandDown.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main()
-{
+{/*
 	int i,j,n;
 	printf("enter data:\n");
 	scanf("%d",&n);
@@ -11,4 +11,19 @@ int main()
 	for(i=(n+1);i>0;i--,printf("\n"))
 		for(j=1;j<=i;j++)
 			printf("*");
+
+*/
+    	int n,A,i,j;
+    	printf("Enter n value: ");
+    	scanf("%d", &n);
+    	for (i = -n; i <= n; i++,printf("\n")) 
+    	{	
+		A=(i<0)?-i:i;
+        	for (j=1; j <=n; j++) 
+		{
+			A<j?printf(" "):printf(" %c",'A'+ j -1); 
+        	}
+    	}
+    return 0;
+
 }
--- loop/pattern/rightangleChar.c
+++ loop/pattern/rightangleChar.c
@@ -2,10 +2,13 @@
 #include<stdlib.h>
 int main()
 {
-	int i,j,n;
+	int i,j,n,A;
 	printf("enter num :\n");
 	scanf("%d",&n);
 	for(i=1;i<=n;i++,printf("\n"))
 		for(j=1;j<=i;j++)
 			printf("%c",64+j);
+	/*for(i=n;i>=0;i--,printf("\n"))
+		for(j=1;j<=i;j++)
+			printf("%c",64+j);*/
 }
